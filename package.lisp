78d2e6ee27297f743b4f4817f0a4b459e969f807
ingram <ingram@ixion.gateway.2wire.net>
2011-04-21 01:34:24 -0400

    -Added inline-css to the package export list

--- package.lisp
+++ package.lisp
@@ -1,5 +1,5 @@
 (defpackage :cl-css
   (:nicknames "MINI-CSS")
   (:use :cl)
-  (:export :css :compile-css)
+  (:export :css :inline-css :compile-css)
   (:documentation "A non-validating, inline CSS generator for common lisp"))
